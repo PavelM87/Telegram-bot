f389552123c0333e252e1dee2fc726a401a8609e
PavelM87 <58486727+PavelM87@users.noreply.github.com>
2021-01-24 16:11:54 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+token = '*****YOUR*TOKEN*****'  # your token by bot_father 
+secret = 'j5j4k4h5h5jkkkgjgbf9' # random set of letters and numbers
+my_url = 'https://**********.pythonanywhere.com/' # your address for example on pythonanywhere.com
