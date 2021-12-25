1361d3e7a924aeffe3236b6def5045d556323bee
PavelM87 <mospv87@gmail.com>
2021-12-26 00:23:35 +0300

    Migrated to telethon library

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
-token = '*****YOUR*TOKEN*****'  # your token by bot_father 
-secret = 'j5j4k4h5h5jkkkgjgbf9' # random set of letters and numbers
-my_url = 'https://**********.pythonanywhere.com/' # your address for example on pythonanywhere.com
+TOKEN = '*****YOUR*TOKEN*****'  # your token by bot_father 
+
+# These example values won't work. You must get your own api_id and
+# api_hash from https://my.telegram.org, under API Development.
+API_ID = 12345
+API_HASH = '0123456789abcdef0123456789abcdef' 
--- main.py
+++ main.py
@@ -1,85 +1,48 @@
-import telebot
 import requests
+from telethon import TelegramClient, events, Button
 from bs4 import BeautifulSoup
-from telebot import types
-from flask import Flask, request
-from config import token, secret, my_url
+from config import TOKEN, API_ID, API_HASH
 
+bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=TOKEN)
 
-url = my_url + secret
-
-bot = telebot.TeleBot(token, threaded=False)
-bot.remove_webhook()
-bot.set_webhook(url=url)
-
-app = Flask(__name__)
-
-keyboard = types.ReplyKeyboardMarkup(True)
-keyboard.row('Случайный пирожок','Отборный пирожок')
-keyboard.row('Источник пирожков')
-
-def get_random():
-    url = 'http://perashki.ru/Piro/Random/'
+def get_poem(address: str = '') -> str:
+    url = f'http://perashki.ru{address}'
     r = requests.get(url).text
+    
     soup = BeautifulSoup(r, 'lxml')
     result = soup.find('div', class_='Text').text
     author = soup.find('div', class_='Author').find('a').text
     date = soup.find('span', class_='date').text
     return f'''{result}
-
     Автор: {author:10}
     Дата публикации: {date}'''
 
-def get_best():
-    url = 'http://perashki.ru'
-    r = requests.get(url).text
-    soup = BeautifulSoup(r, 'lxml')
-    result = soup.find('div', class_='Text').text
-    author = soup.find('div', class_='Author').find('a').text
-    date = soup.find('span', class_='date').text
-    return f'''{result}
 
-    Автор: {author:10}
-    Дата публикации: {date}'''
-
-@app.route('/' + secret, methods=['POST'])
-def webhook():
-    update = telebot.types.Update.de_json(request.stream.read().decode('utf-8'))
-    bot.process_new_updates([update])
-    return 'ok', 200
-
-@bot.message_handler(commands=['start'])
-def start(m):
-    bot.send_message(m.chat.id, f'''Бот присылает случайное четверостишие - "пирожок" с сайта perashki.ru.
+@bot.on(events.NewMessage(pattern='/start'))
+async def send_welcome(event):
+    await bot.send_message(
+        event.peer_id.user_id,
+        f'''Бот присылает случайное четверостишие - "пирожок" с сайта perashki.ru.
     Используй кнопки для взаимодействия с ботом.
-    Случайный пирожок - четверостишие из раздела "Рандом" сайта.
-    Отборный пирожок - четверостишие из раздела "Наше лучшее".
-    Источник пирожков - ссылка на сайт perashki.ru''')
-    send(m.chat.id, get_best())
+    Случайный пирожок - стих из раздела "Рандом" сайта.
+    Отборный пирожок - стих из раздела "Наше лучшее".
+    Источник пирожков - ссылка на сайт perashki.ru''',
+        buttons=[[Button.text('Случайный пирожок', resize=True), Button.text('Отборный пирожок', resize=True)],
+                 [Button.text('Источник пирожков', resize=True)]]
+    )
 
-@bot.message_handler(commands=['help'])
-def help(m):
-    bot.send_message(m.chat.id, f'''Бот присылает случайное четверостишие - "пирожок" с сайта perashki.ru.
-    Используй кнопки для взаимодействия с ботом.
-    Случайный пирожок - четверостишие из раздела "Рандом" сайта.
-    Отборный пирожок - четверостишие из раздела "Наше лучшее".
-    Источник пирожков - ссылка на сайт perashki.ru''')
 
-@bot.message_handler(content_types=['text'])
-def main(message):
-    id = message.chat.id
-    msg = message.text
-    if msg == 'Случайный пирожок':
-        send(id, get_random())
+@bot.on(events.NewMessage)
+async def main(event):
+    msg = event.text
+    user_id = event.peer_id.user_id
+    if msg == '/help':
+        await bot.send_message(user_id, 'Используй кнопки для взаимодействия с ботом')
+    elif msg == 'Случайный пирожок':
+        await bot.send_message(user_id, get_poem('/Piro/Random/'))
     elif msg == 'Отборный пирожок':
-        send(id, get_best())
+        await bot.send_message(user_id, get_poem())
     elif msg == 'Источник пирожков':
-        send(id, 'perashki.ru')
-    else:
-        send(id, 'Используй кнопки!')
-
-
-
-def send(id, text):
-    bot.send_message(id, text, reply_markup=keyboard)
+        await bot.send_message(user_id, 'perashki.ru')
+bot.run_until_disconnected()
 
